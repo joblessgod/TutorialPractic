aeb27edb2a19ea34824d3cb63fa41d0af0230d43
Rusky <91284372+RuskyDev@users.noreply.github.com>
2024-09-17 15:41:32 +0500

    Update TutorialCommands.java

--- src/com/joblessgod/tutorial/Commands/TutorialCommands.java
+++ src/com/joblessgod/tutorial/Commands/TutorialCommands.java
@@ -8,48 +8,51 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class TutorialCommands implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("Only players can run this commands!");
-            return true;}
-            Player player = (Player) sender;
-        // /heal
-        if (cmd.getName().equalsIgnoreCase("heal")) {
+            sender.sendMessage("Only players can execute this command!");
+            return true;
+        }
+        
+        Player player = (Player) sender;
 
+        if (cmd.getName().equalsIgnoreCase("heal")) {
             double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
             player.setHealth(maxHealth);
             player.sendMessage("§e§l(!) §eYou have been healed!");
             return true;
         }
 
-        // /feed
         if (cmd.getName().equalsIgnoreCase("feed")) {
             player.setFoodLevel(20);
-            player.sendMessage("§e§l(!) §eYou have been feed!");
+            player.sendMessage("§e§l(!) §eYou have been fed!");
             return true;
         }
 
-        // /farmtime cow 5
-        else if (cmd.getName().equalsIgnoreCase("farmtime")) {
-            if (args.length >= 2){
+        if (cmd.getName().equalsIgnoreCase("farmtime")) {
+            if (args.length >= 2) {
                 try {
-                EntityType entity = EntityType.valueOf(args[0].toUpperCase());
-                int amount = Integer.parseInt(args[1]);
-                for (int i = 0; i < amount; i++) {
-                    player.getWorld().spawnEntity(player.getLocation(), entity);
-                }
+                    EntityType entity = EntityType.valueOf(args[0].toUpperCase());
+                    int amount = Integer.parseInt(args[1]);
 
-                }catch (IllegalArgumentException e) {
-                player.sendMessage("§c§l(!) §cThat's not a valid entity!");
+                    for (int i = 0; i < amount; i++) {
+                        player.getWorld().spawnEntity(player.getLocation(), entity);
+                    }
+                    player.sendMessage("§e§l(!) §eEntities spawned successfully!");
+
+                } catch (IllegalArgumentException e) {
+                    player.sendMessage("§c§l(!) §cThat's not a valid entity or amount!");
                 }
+                return true;
+            } else {
+                player.sendMessage("§c§l(!) §cUsage: /farmtime <mob> <amount>");
+                return true;
             }
-
-        } else {
-            player.sendMessage("§c§l(!) §c/farmtime <mob> <amount>");
         }
 
-        return true;
+        player.sendMessage("§c§l(!) §cUnknown command. Use /help for a list of commands.");
+        return false;
     }
-
 }
